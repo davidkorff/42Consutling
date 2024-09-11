3c28f6435e0afa64a3bac10515bd95a91da959c3
davidkorff <davidkorff@gmail.com>
2024-09-11 11:27:53 -0400

    Your commit message

---
+++ assets/js/script.js
@@ -0,0 +1,230 @@
+'use strict';
+
+
+
+// element toggle function
+const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
+
+
+
+// sidebar variables
+const sidebar = document.querySelector("[data-sidebar]");
+const sidebarBtn = document.querySelector("[data-sidebar-btn]");
+
+// sidebar toggle functionality for mobile
+sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
+
+
+
+// testimonials variables
+const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
+const modalContainer = document.querySelector("[data-modal-container]");
+const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
+const overlay = document.querySelector("[data-overlay]");
+
+// modal variable
+const modalImg = document.querySelector("[data-modal-img]");
+const modalTitle = document.querySelector("[data-modal-title]");
+const modalText = document.querySelector("[data-modal-text]");
+
+// modal toggle function
+const testimonialsModalFunc = function () {
+  modalContainer.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
+
+// add click event to all modal items
+for (let i = 0; i < testimonialsItem.length; i++) {
+
+  testimonialsItem[i].addEventListener("click", function () {
+
+    modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
+    modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
+    modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
+    modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
+
+    testimonialsModalFunc();
+
+  });
+
+}
+
+// add click event to modal close button
+modalCloseBtn.addEventListener("click", testimonialsModalFunc);
+overlay.addEventListener("click", testimonialsModalFunc);
+
+
+
+// custom select variables
+const select = document.querySelector("[data-select]");
+const selectItems = document.querySelectorAll("[data-select-item]");
+const selectValue = document.querySelector("[data-selecct-value]");
+const filterBtn = document.querySelectorAll("[data-filter-btn]");
+
+select.addEventListener("click", function () { elementToggleFunc(this); });
+
+// add event in all select items
+for (let i = 0; i < selectItems.length; i++) {
+  selectItems[i].addEventListener("click", function () {
+
+    let selectedValue = this.innerText.toLowerCase();
+    selectValue.innerText = this.innerText;
+    elementToggleFunc(select);
+    filterFunc(selectedValue);
+
+  });
+}
+
+// filter variables
+const filterItems = document.querySelectorAll("[data-filter-item]");
+
+const filterFunc = function (selectedValue) {
+
+  for (let i = 0; i < filterItems.length; i++) {
+
+    if (selectedValue === "all") {
+      filterItems[i].classList.add("active");
+    } else if (selectedValue === filterItems[i].dataset.category) {
+      filterItems[i].classList.add("active");
+    } else {
+      filterItems[i].classList.remove("active");
+    }
+
+  }
+
+}
+
+// add event in all filter button items for large screen
+let lastClickedBtn = filterBtn[0];
+
+for (let i = 0; i < filterBtn.length; i++) {
+
+  filterBtn[i].addEventListener("click", function () {
+
+    let selectedValue = this.innerText.toLowerCase();
+    selectValue.innerText = this.innerText;
+    filterFunc(selectedValue);
+
+    lastClickedBtn.classList.remove("active");
+    this.classList.add("active");
+    lastClickedBtn = this;
+
+  });
+
+}
+
+
+
+// contact form variables
+const form = document.querySelector("[data-form]");
+const formInputs = document.querySelectorAll("[data-form-input]");
+const formBtn = document.querySelector("[data-form-btn]");
+
+// add event to all form input field
+for (let i = 0; i < formInputs.length; i++) {
+  formInputs[i].addEventListener("input", function () {
+
+    // check form validation
+    if (form.checkValidity()) {
+      formBtn.removeAttribute("disabled");
+    } else {
+      formBtn.setAttribute("disabled", "");
+    }
+
+  });
+}
+
+
+
+// page navigation variables
+const navigationLinks = document.querySelectorAll("[data-nav-link]");
+const pages = document.querySelectorAll("[data-page]");
+
+// add event to all nav link
+for (let i = 0; i < navigationLinks.length; i++) {
+  navigationLinks[i].addEventListener("click", function () {
+
+    for (let i = 0; i < pages.length; i++) {
+      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+        pages[i].classList.add("active");
+        navigationLinks[i].classList.add("active");
+        window.scrollTo(0, 0);
+      } else {
+        pages[i].classList.remove("active");
+        navigationLinks[i].classList.remove("active");
+      }
+    }
+
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  var form = document.querySelector('[data-form]');
+  var modal = document.getElementById('thankYouModal');
+
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    var fullName = document.querySelector('input[name="fullname"]').value;
+    var email = document.querySelector('input[name="email"]').value;
+    var message = document.querySelector('textarea[name="message"]').value;
+
+    var payload = {
+      "fullName": fullName,
+      "email": email,
+      "message": message
+    }
+
+    // Convert payload to URL search parameters
+    var urlParams = new URLSearchParams(payload).toString();
+
+    fetch('https://hooks.zapier.com/hooks/catch/4360879/36rm455/?' + urlParams, {
+      method: 'GET',
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+
+      // Show the modal
+      modal.style.display = "block";
+
+      // Set a timer to hide the modal after 2 seconds
+      setTimeout(function() {
+        modal.style.display = "none";
+      }, 2000);
+
+      // Reset the form
+      form.reset();
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  });
+});
+
+
+window.onload = function() {
+  // Get the user agent, language, screen size, color depth and timezone offset
+  const data = {
+    userAgent: navigator.userAgent,
+    language: navigator.language,
+    screenSize: window.screen.width + 'x' + window.screen.height,
+    colorDepth: window.screen.colorDepth,
+    timezoneOffset: new Date().getTimezoneOffset()
+  };
+
+  // Create the URL with the parameters
+  const params = new URLSearchParams(data).toString();
+  const webhookURL = 'https://hooks.zapier.com/hooks/catch/4360879/3dm10dd/';
+
+  // Send a GET request to the webhook
+  fetch(`${webhookURL}?${params}`, { method: 'GET' })
+    .then(response => {
+      if (response.ok) {
+        console.log('Data sent successfully');
+      } else {
+        throw new Error('Request failed');
+      }
+    })
+    .catch(error => console.error('Failed to send data: ', error));
+};
